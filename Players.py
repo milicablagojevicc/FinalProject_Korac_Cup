024e84d567fa2ba14db4b75326134a9dda225afa
milicablagojevicc <milicab48@gmail.com>
2025-02-20 10:01:54 +0100

    Start of the project - defining the teams

---
+++ Players.py
@@ -0,0 +1,9 @@
+#To make the game more interesting, we will include top 12 of all time that played in these clubs
+#Crvena Zvezda Meridianbet Belgrade
+#Partizan Mozzart Bet Belgrade
+#FMP Soccerbet
+#Mega MIS
+#Mozart
+#Spartak Office Shoes
+#Vojvodina
+#Dunav Stari Banovc
---
+++ Teams.py
@@ -0,0 +1,10 @@
+#Teams competing in the Radivoje Korać Cup 2025 included:
+#Crvena Zvezda Meridianbet Belgrade
+#Partizan Mozzart Bet Belgrade
+#FMP Soccerbet
+#Mega MIS
+#Mozart
+#Spartak Office Shoes
+#Vojvodina
+#Dunav Stari Banovc
+
